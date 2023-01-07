6973b1e6338d7a08c410ac224438b8e38ea796c9
ChalindKodikara <chalindu.18@cse.mrt.ac.lk>
2023-01-07 13:59:03 +0530

    Initial commit

---
+++ Assets/ARPlacement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    // These 2 public, because we need to add them from the project
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+
+    private GameObject spawnedObject; // Object to store our spawned object
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // need to update placement indicator, placement pose and spawn 
+    void Update()
+    {   
+        // If the conditions are true to place the AR object
+        if(spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+
+        // Otherwise keep updating pose and position
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+
+    }
+    void UpdatePlacementIndicator()
+    {
+        if(spawnedObject == null && placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else // AR object is spawned
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f)); // Position of the screen center
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    // Place AR Object
+    void ARPlaceObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+    }
+
+
+}
